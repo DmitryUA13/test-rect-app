23920250d5759bb39831290a4a079c102605e751
DmitryUA13 <bot2biz@gmail.com>
2023-01-02 19:02:04 +0200

    1

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -2,7 +2,11 @@ import styles from './Buttom.module.scss'
 
 
 export default function Buttons () {
+    function onCkick() {
+        alert("Is working!!");
+    }
+
     return (
-        <button  className={styles.ButtonBlue}>Click Me!!</button>
+        <button  className={styles.ButtonBlue} onClick={onCkick}>Click Me!!</button>
     )
 }
